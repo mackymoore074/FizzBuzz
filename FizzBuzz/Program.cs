eaaa5ce1c94004bd94f6fd5a7ed5813223dfd367
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-13 11:46:28 -0700

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public string FizzBuzz(int number)
+    {
+        var answer = "";
+
+        if (number % 3 == 0)
+        {
+            answer = "fizz";
+        }
+        if (number % 5 == 0)
+        {
+            answer += "Buzz";
+        }
+        return answer;
+    }
+}
