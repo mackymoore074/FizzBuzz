7a3dd35d3ff207e382ffcf670f4c054ec2ef5194
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-27 12:40:04 -0700

    done

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,7 +7,10 @@
     public string FizzBuzz(int number)
     {
         var answer = "";
-
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            answer = "fizzbuzz";
+        }
         if (number % 3 == 0)
         {
             answer = "fizz";
